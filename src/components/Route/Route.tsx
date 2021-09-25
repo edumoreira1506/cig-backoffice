b1de4a498a728354b1137fa9d721832ac1559835
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-25 17:55:39 -0300

    Redirect to login url

--- src/components/Route/Route.tsx
+++ src/components/Route/Route.tsx
@@ -2,6 +2,7 @@ import React, { ComponentType } from 'react'
 import { Route as ReactRouterRoute, RouteProps as ReactRouterRouteProps } from 'react-router'
 
 import useAuth from 'hooks/useAuth'
+import { LOGIN_URL } from 'constants/url'
 
 export interface RouteProps extends ReactRouterRouteProps {
   component: ComponentType;
@@ -17,8 +18,8 @@ export default function Route({ component: Component, ...rest }: RouteProps) {
         isAuthenticated ? (
           <Component />
         ) : (
-          <h1>DeuRuim</h1>
-        )
+          window.location.assign(LOGIN_URL)
+        ) as any
       }
     />
   )
