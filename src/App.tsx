b37faf9b979d29ee416901a6e2b600f454e39aa6
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-25 17:09:03 -0300

    Setup container

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,15 @@
-export { default } from './routes'
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { Container } from '@cig-platform/ui'
+
+import Routes from './routes'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Container title="" items={[]} onMenuClick={console.log} user={{ image: '', name: 'Eduardo' }}>
+        <Routes />
+      </Container>
+    </BrowserRouter>
+  )
+}
