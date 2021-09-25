7f957a20b129ec2168c4f6c90ef971969427ff0a
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-25 17:57:18 -0300

    Add enum to Routes

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
+import { Routes } from 'constants/routes'
 import Route from 'components/Route/Route'
 import Home from 'pages/Home/Home'
 
 export default function Router() {
   return (
     <>
-      <Route path="/" component={Home} />
+      <Route path={Routes.Home} component={Home} />
     </>
   )
 }
