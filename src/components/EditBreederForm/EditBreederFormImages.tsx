413959033ddbbffbf31a31d3128d9cfc259852b4
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-09 16:26:58 -0300

    Add image gallery

--- src/components/EditBreederForm/EditBreederFormImages.tsx
+++ src/components/EditBreederForm/EditBreederFormImages.tsx
@@ -1,6 +1,7 @@
-import React, { useMemo } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { FileImagesCarousel } from '@cig-platform/ui'
+import Modal from 'react-modal'
+import { Colors, FileImagesCarousel, ImageGallery } from '@cig-platform/ui'
 
 import { useEditBreederSelector } from '../../contexts/EditBreederContext/EditBreederContext'
 import { selectImages } from '../../contexts/EditBreederContext/editBreederSelectors'
@@ -8,22 +9,54 @@ import { createImageUrl } from '../../utils/s3'
 import { S3Subfolders, S3Folders } from '../../constants/s3'
 
 export default function EditBreederFormImages() {
+  const [isOpenGallery, setIsOpenGallery] = useState(false)
+
+  const [galleryIndex, setGalleryIndex] = useState(0)
+
+  const closeGallery = useCallback(() => setIsOpenGallery(false), [])
+  const openGallery = useCallback(() => setIsOpenGallery(true), [])
+
   const images = useEditBreederSelector(selectImages)
 
   const { t } = useTranslation()
 
-  const formattedImages = useMemo(() => images.map((image) => ({
+  const formattedImagesOfCarousel = useMemo(() => images.map((image) => ({
     src: createImageUrl({ folder: S3Folders.Breeders, fileName: image.imageUrl, subfolder: S3Subfolders.Images }),
     alt: image.imageUrl
   })), [images])
 
+  const formattedImagesOfGallery = useMemo(() => images.map((image) => ({
+    original: createImageUrl({ folder: S3Folders.Breeders, fileName: image.imageUrl, subfolder: S3Subfolders.Images }),
+    thumbnail: createImageUrl({ folder: S3Folders.Breeders, fileName: image.imageUrl, subfolder: S3Subfolders.Images }),
+  })), [images])
+
+  const handleClickImage = useCallback((imageSrc: string) => {
+    const imageIndex = formattedImagesOfCarousel.findIndex(image => image.src === imageSrc)
+
+    setGalleryIndex(imageIndex)
+    openGallery()
+  }, [formattedImagesOfCarousel, openGallery])
+
   return (
-    <FileImagesCarousel
-      images={formattedImages}
-      onClickImage={() => alert('clicou na ibagem')}
-      onUpload={() => alert('upload!')}
-      onDeleteImage={() => alert('deletando')}
-      uploadMessage={t('common.select-files')}
-    />
+    <>
+      <FileImagesCarousel
+        images={formattedImagesOfCarousel}
+        onClickImage={handleClickImage}
+        onUpload={() => alert('upload!')}
+        onDeleteImage={() => alert('deletando')}
+        uploadMessage={t('common.select-files')}
+      />
+      <Modal
+        isOpen={isOpenGallery}
+        onRequestClose={closeGallery}
+        style={{ overlay: { background: Colors.BlackTransparent, zIndex: 1000 } }}
+      >
+        <ImageGallery
+          showPlayButton={false}
+          items={formattedImagesOfGallery}
+          startIndex={galleryIndex}
+        />
+      </Modal>
+    </>
   )
 }
