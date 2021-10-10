7d7747eb6c0f1d2e7e4fcb349699a21684f3df53
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-10 18:30:44 -0300

    Fix tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,2 +1,12 @@
 import '@testing-library/jest-dom'
 import 'config/i18n'
+
+window.matchMedia =
+  window.matchMedia ||
+  function() {
+    return {
+      matches: false,
+      addListener: function() { return undefined },
+      removeListener: function() { return undefined }
+    }
+  }
