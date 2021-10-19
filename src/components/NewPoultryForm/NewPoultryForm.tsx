4122d1ba5da88cbd02e9d80bd8f0a8239939be5f
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-18 22:06:27 -0300

    Add request to save poultry

--- src/components/NewPoultryForm/NewPoultryForm.tsx
+++ src/components/NewPoultryForm/NewPoultryForm.tsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import { IPoultry } from '@cig-platform/types'
+
+import { preventDefaultHandler } from 'utils/dom'
 
 import { StyledForm, StyledFormField, StyledSubtitle } from './NewPoultryForm.styles'
 import NewPoultryFormBirthDate from './NewPoultryFormBirthDate'
 import NewPoultryFormColors from './NewPoultryFormColors'
 import NewPoultryFormType from './NewPoultryFormType'
 import NewPoultryFormVideos from './NewPoultryFormVideos'
+import NewPoultryFormSubmitButton from './NewPoultryFormSubmitButton'
 
-export default function NewPoultryForm() {
+export interface NewPoultryFormProps {
+  onSubmit: (poultry: Partial<IPoultry>) => void;
+}
+
+export default function NewPoultryForm({ onSubmit }: NewPoultryFormProps) {
   const { t } = useTranslation()
 
   return (
-    <StyledForm>
+    <StyledForm onSubmit={preventDefaultHandler}>
       <StyledFormField>
         <NewPoultryFormType />
       </StyledFormField>
@@ -22,6 +30,9 @@ export default function NewPoultryForm() {
       <NewPoultryFormColors />
       <StyledSubtitle>{t('poultry.fields.videos')}</StyledSubtitle>
       <NewPoultryFormVideos />
+      <StyledFormField>
+        <NewPoultryFormSubmitButton onSubmit={onSubmit} />
+      </StyledFormField>
     </StyledForm>
   )
 }
