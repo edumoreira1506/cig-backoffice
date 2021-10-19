c446cf915b99841b8d42715d35a4f4e0aadaa388
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-19 18:43:33 -0300

    Add poultry form to edit poultry container

--- src/containers/EditPoultryContainer/EditPoultryContainer.tsx
+++ src/containers/EditPoultryContainer/EditPoultryContainer.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useHistory, useParams } from 'react-router'
+
+import { Routes } from 'constants/routes'
+import { success } from 'utils/alert'
+import useEditPoultry from 'hooks/useEditPoultry'
+import PoultryForm from 'components/PoultryForm/PoultryForm'
 
 export default function EditPoultryContainer() {
+  const { t } = useTranslation()
+
+  const history = useHistory()
+
+  const { poultryId } = useParams<{ poultryId: string }>()
+
+  const handleSuccess = useCallback(() => {
+    success(t('common.saved'), t, () => history.push(Routes.ListPoultries))
+  }, [t, history])
+
+  const editPoultry = useEditPoultry({ onSuccess: handleSuccess, poultryId })
+
   return (
-    <h1>EditPoultryContainer</h1>
+    <PoultryForm onSubmit={editPoultry} />
   )
 }
