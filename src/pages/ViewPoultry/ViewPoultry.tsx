008908bedd5ac64d0749fa27175c79b3e0b7f232
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-24 12:31:16 -0300

    Change view poultry layout

--- src/pages/ViewPoultry/ViewPoultry.tsx
+++ src/pages/ViewPoultry/ViewPoultry.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import { IPoultry, IPoultryImage } from '@cig-platform/types'
+
+import BackofficeBffService from 'services/BackofficeBffService'
+import useBreeder from 'hooks/useBreeder'
+import useAuth from 'hooks/useAuth'
+import MicroFrontend from 'components/MicroFrontend/MicroFrontend'
+import { POULTRY_PAGE_URL } from 'constants/url'
 
 export default function ViewPoultry() {
+  const [poultry, setPoultry] = useState<undefined | IPoultry & { images: IPoultryImage[] }>()
+
+  const { poultryId } = useParams<{ poultryId: string }>()
+
+  const breeder = useBreeder()
+
+  const { token } = useAuth()
+
+  useEffect(() => {
+    if (!poultryId || !breeder) return
+   
+    (async () => {
+      const poultryData = await BackofficeBffService.getPoultry(breeder.id, poultryId, token)
+
+      setPoultry(poultryData)
+    })()
+  }, [poultryId, token, breeder])
+
+  if (!poultry) return null
+
   return (
-    <h1>ViewPoultry</h1>
+    <div id="poultry-preview">
+      <MicroFrontend
+        name="PoultryPage"
+        host={POULTRY_PAGE_URL}
+        containerId="poultry-preview"
+        poultry={poultry}
+        images={poultry.images}
+      />
+    </div>
   )
 }
