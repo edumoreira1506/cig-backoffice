c305ab1ac6a957d1b7b844bb81ee653d9c2e1e26
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-25 22:38:53 -0300

    Add api request

--- src/containers/NewRegisterContainer/NewRegisterContainer.tsx
+++ src/containers/NewRegisterContainer/NewRegisterContainer.tsx
@@ -1,15 +1,23 @@
 import React, { useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Tabs } from '@cig-platform/ui'
+import { Button, Tabs } from '@cig-platform/ui'
 
-import { useRegisterDispatch } from 'contexts/RegisterContext/RegisterContext'
+import { useRegisterDispatch, useRegisterSelector } from 'contexts/RegisterContext/RegisterContext'
 import { setType } from 'contexts/RegisterContext/registerActions'
 import RegisterImageForm from 'components/RegisterImageForm/RegisterImageForm'
+
+import { StyledButton } from './NewRegisterContainer.styles'
+import useSaveRegister from 'hooks/useSaveRegister'
+import { selectType, selectDescription, selectFiles } from 'contexts/RegisterContext/registerSelectors'
 
 const registerTypes = ['IMAGENS', 'MEDIÇÃO', 'PESAGEM', 'VACINAÇÃO']
 
 export default function NewRegisterContainer() {
   const { t } = useTranslation()
+
+  const type = useRegisterSelector(selectType)
+  const description = useRegisterSelector(selectDescription)
+  const files = useRegisterSelector(selectFiles)
 
   const dispatch = useRegisterDispatch()
 
@@ -17,18 +25,31 @@ export default function NewRegisterContainer() {
     dispatch(setType(registerTypes[newTypeIndex]))
   }, [dispatch])
 
+  const saveRegister = useSaveRegister({ onSuccess: () => alert('deu bão!') })
+
+  const handleSave = useCallback(() => {
+    saveRegister({ description, type }, files.map(file => file.file))
+  }, [saveRegister, type, description, files])
+
   return (
-    <Tabs setTab={handleChangeType}>
-      <RegisterImageForm title={t('register.fields.type.images')} />
-      <div title={t('register.fields.type.measurement')}>
+    <>
+      <Tabs setTab={handleChangeType}>
+        <RegisterImageForm title={t('register.fields.type.images')} />
+        <div title={t('register.fields.type.measurement')}>
         Medição
-      </div>
-      <div title={t('register.fields.type.weighing')}>
+        </div>
+        <div title={t('register.fields.type.weighing')}>
         Pesagem
-      </div>
-      <div title={t('register.fields.type.vaccination')}>
+        </div>
+        <div title={t('register.fields.type.vaccination')}>
         Vacinação
-      </div>
-    </Tabs>
+        </div>
+      </Tabs>
+      <StyledButton>
+        <Button onClick={handleSave}>
+          {t('common.save')}
+        </Button>
+      </StyledButton>
+    </>
   )
 }
