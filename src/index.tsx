cdc644081df4fb5ccf2b1244242944045d9128ec
Eduardo Moreira <contato@eduardoem.com.br>
2021-11-23 21:09:02 -0300

    Add react-slick styles import

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom'
 
 import App from 'App'
 
+import 'slick-carousel/slick/slick.css' 
+import 'slick-carousel/slick/slick-theme.css'
+
 import 'config/i18n'
 
 ReactDOM.render(
