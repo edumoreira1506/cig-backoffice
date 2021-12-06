0a6f53b2c81c10aebac41148890d3bd3b5d1a010
Eduardo Moreira <contato@eduardoem.com.br>
2021-12-05 23:27:40 -0300

    Add ContentSearchService

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -2,6 +2,7 @@ export const LOGIN_URL = String(process.env.REACT_APP_LOGIN_URL)
 
 export const AUTH_BFF_API_URL = String(process.env.REACT_APP_AUTH_BFF_API_URL)
 export const BACKOFFICE_BFF_API_URL = String(process.env.REACT_APP_BACKOFFICE_BFF_API_URL)
+export const CONTENT_SEARCH_API_URL = String(process.env.REACT_APP_CONTENT_SEARCH_API_URL)
 
 export const S3_BUCKET_URL = String(process.env.REACT_APP_S3_BUCKET_URL)
 
