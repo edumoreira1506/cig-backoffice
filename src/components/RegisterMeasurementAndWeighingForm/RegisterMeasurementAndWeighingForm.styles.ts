beb027fac7b14aa2dc4982628d641b213c5fe255
Eduardo Moreira <contato@eduardoem.com.br>
2021-12-19 14:01:30 -0300

    Add measurement and weighing form

---
+++ src/components/RegisterMeasurementAndWeighingForm/RegisterMeasurementAndWeighingForm.styles.ts
@@ -0,0 +1,39 @@
+import { createMinWidthMediaQuery } from '@cig-platform/ui'
+import styled from 'styled-components'
+
+export const StyledContainer = styled.div`
+  width: 100%;
+
+  ${createMinWidthMediaQuery(`
+    width: 60%;
+    margin: 0 auto;
+  `)}
+`
+
+export const StyledButton = styled.div`
+  ${createMinWidthMediaQuery(`
+    width: 60%;
+    margin: 0 auto;
+  `)}
+`
+
+export const StyledForm = styled.form`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 20px;
+  flex-wrap: wrap;
+`
+
+export const StyledField = styled.div`
+  width: 30%;
+
+  ${createMinWidthMediaQuery(`
+    width: 32%;
+  `)}
+`
+
+export const StyledDescriptionField = styled.div`
+  width: 100%;
+  margin-top: 15px;
+`
--- src/contexts/RegisterContext/registerSelectors.ts
+++ src/contexts/RegisterContext/registerSelectors.ts
@@ -13,3 +13,11 @@ export const selectVaccinationName = (state: RegisterState) => selectVaccination
 export const selectVaccinationDose = (state: RegisterState) => selectVaccination(state).dose
 
 export const selectVaccinationDate = (state: RegisterState) => selectVaccination(state).date
+
+export const selectMeasurementAndWeighing = (state: RegisterState) => state.measurementAndWeighing
+
+export const selectMeasurement = (state: RegisterState) => selectMeasurementAndWeighing(state).measurement
+
+export const selectWeight = (state: RegisterState) => selectMeasurementAndWeighing(state).weight
+
+export const selectMeasurementAndWeighingDate = (state: RegisterState) => selectMeasurementAndWeighing(state).date
