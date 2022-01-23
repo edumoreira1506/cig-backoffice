94525a8784562b85360b49d7d1ad0489d48b824f
Eduardo Moreira <contato@eduardoem.com.br>
2022-01-22 21:23:45 -0300

    Update container declararion

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,4 +1,5 @@
 export const LOGIN_URL = String(process.env.REACT_APP_LOGIN_URL)
+export const LOGO_URL = String(process.env.REACT_APP_LOGO_URL)
 
 export const AUTH_BFF_API_URL = String(process.env.REACT_APP_AUTH_BFF_API_URL)
 export const BACKOFFICE_BFF_API_URL = String(process.env.REACT_APP_BACKOFFICE_BFF_API_URL)
