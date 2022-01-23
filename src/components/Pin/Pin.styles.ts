85641067962ff7b9144cd91238a25bb4c8bae7b7
Eduardo Moreira <contato@eduardoem.com.br>
2022-01-23 12:07:42 -0300

    Change map pin color

--- src/components/Pin/Pin.styles.ts
+++ src/components/Pin/Pin.styles.ts
@@ -2,6 +2,6 @@ import styled from 'styled-components'
 import { Colors } from '@cig-platform/ui'
 
 export const StyledContainer = styled.div`
-  color: ${Colors.White};
+  color: ${Colors.DarkBlue};
   font-size: 2em;
 `
