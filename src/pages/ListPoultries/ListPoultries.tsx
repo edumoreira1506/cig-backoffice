87326f5063b2cd84006e920850e165307de06969
Eduardo Moreira <contato@eduardoem.com.br>
2022-01-23 22:52:33 -0300

    Change list poultries to micro front end

--- src/pages/ListPoultries/ListPoultries.tsx
+++ src/pages/ListPoultries/ListPoultries.tsx
@@ -1,124 +1,53 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useHistory } from 'react-router'
-import { IPoultry } from '@cig-platform/types'
-import { Button, PoultriesCarousel } from '@cig-platform/ui'
+import { Button } from '@cig-platform/ui'
+import MicroFrontend from '@cig-platform/microfrontend-helper'
 
 import Main from 'components/Main/Main'
 import PageTitle from 'components/PageTitle/PageTitle'
-import { useAppDispatch } from 'contexts/AppContext/AppContext'
-import { setError } from 'contexts/AppContext/appActions'
-import BackofficeBffService from 'services/BackofficeBffService'
 import useBreeder from 'hooks/useBreeder'
-import useAuth from 'hooks/useAuth'
 import { Routes } from 'constants/routes'
+import { POULTRY_CAROUSELS_PAGE_URL } from 'constants/url'
 
 import {
   StyledNewPoultry,
-  StyledPoultriesCarousel,
-  StyledPoultriesCarouselTitle,
+  StyledPoultries
 } from './ListPoultries.styles'
-import { createImageUrl } from 'utils/s3'
-import { S3Folders, S3Subfolders } from 'constants/s3'
-
-interface Poultry extends IPoultry {
-  mainImage: string;
-}
 
 export default function ListPoultriesPage() {
   const { t } = useTranslation()
-
-  const dispatch = useAppDispatch()
 
   const history = useHistory()
 
   const breeder = useBreeder()
 
-  const { token } = useAuth()
-
-  const [poultries, setPoultries] = useState<{
-    reproductives: Poultry[];
-    matrix: Poultry[];
-    male: Poultry[];
-    female: Poultry[];
-  }>({
-    reproductives: [],
-    male: [],
-    matrix: [],
-    female: []
-  })
-
   const handleNavigateToNewPoultry = useCallback(() => {
     history.push(Routes.NewPoultry)
   }, [history])
 
-  const handleNavigateToEditPoultry = useCallback((poultryId: string) => {
+  const handleNavigateToEditPoultry = useCallback(({ poultryId }: { poultryId: string }) => {
     if (poultryId) {
       history.push(Routes.EditPoultry.replace(':poultryId', poultryId))
     }
   }, [history])
 
-  const handleNavigateToViewPoultry = useCallback((poultryId: string) => {
+  const handleNavigateToViewPoultry = useCallback(({ poultryId }: { poultryId: string }) => {
     if (poultryId) {
       history.push(Routes.ViewPoultry.replace(':poultryId', poultryId))
     }
   }, [history])
 
-  useEffect(() => {
-    if (!breeder) return
+  const microFrontendParams = useMemo(() => ({
+    breederId: breeder?.id ?? ''
+  }), [breeder?.id])
 
-    (async () => {
-      try {
-        const newPoultries = await BackofficeBffService.getPoultries(breeder.id, token)
-        const poultries = {
-          reproductives: newPoultries.reproductives.map((poultry) => ({
-            ...poultry,
-            mainImage: poultry.mainImage
-              ? createImageUrl({
-                folder: S3Folders.Poultries,
-                subfolder: S3Subfolders.Images,
-                fileName: poultry.mainImage
-              })
-              : 'https://farmhousepoultry.ca/wp-content/uploads/2016/03/Product_ImageComingSoon_592x592-380x400.jpg'
-          })),
-          matrix: newPoultries.matrix.map((poultry) => ({
-            ...poultry,
-            mainImage: poultry.mainImage
-              ? createImageUrl({
-                folder: S3Folders.Poultries,
-                subfolder: S3Subfolders.Images,
-                fileName: poultry.mainImage
-              })
-              : 'https://farmhousepoultry.ca/wp-content/uploads/2016/03/Product_ImageComingSoon_592x592-380x400.jpg'
-          })),
-          male: newPoultries.male.map((poultry) => ({
-            ...poultry,
-            mainImage: poultry.mainImage
-              ? createImageUrl({
-                folder: S3Folders.Poultries,
-                subfolder: S3Subfolders.Images,
-                fileName: poultry.mainImage
-              })
-              : 'https://farmhousepoultry.ca/wp-content/uploads/2016/03/Product_ImageComingSoon_592x592-380x400.jpg'
-          })),
-          female: newPoultries.female.map((poultry) => ({
-            ...poultry,
-            mainImage: poultry.mainImage
-              ? createImageUrl({
-                folder: S3Folders.Poultries,
-                subfolder: S3Subfolders.Images,
-                fileName: poultry.mainImage
-              })
-              : 'https://farmhousepoultry.ca/wp-content/uploads/2016/03/Product_ImageComingSoon_592x592-380x400.jpg'
-          })),
-        }
+  const microFrontendCallbacks = useMemo<Record<string, any>>(() => ({
+    onViewPoultry: handleNavigateToViewPoultry,
+    onEditPoultry: handleNavigateToEditPoultry
+  }), [handleNavigateToEditPoultry, handleNavigateToViewPoultry])
 
-        setPoultries(poultries)
-      } catch (error) {
-        dispatch(setError(error))
-      }
-    })()
-  }, [breeder])
+  if (!breeder?.id) return null
 
   return (
     <Main>
@@ -130,58 +59,16 @@ export default function ListPoultriesPage() {
           {t('create-poultry')}
         </Button>
       </StyledNewPoultry>
-      {Boolean(poultries.matrix.length) && (
-        <StyledPoultriesCarousel>
-          <StyledPoultriesCarouselTitle>
-            {t('poultry.fields.gender-category.matrix')}
-          </StyledPoultriesCarouselTitle>
-          <PoultriesCarousel
-            onEditPoultry={handleNavigateToEditPoultry}
-            onClickImage={handleNavigateToViewPoultry}
-            onViewPoultry={handleNavigateToViewPoultry}
-            poultries={poultries.matrix}
-          />
-        </StyledPoultriesCarousel>
-      )}
-      {Boolean(poultries.reproductives.length) && (
-        <StyledPoultriesCarousel>
-          <StyledPoultriesCarouselTitle>
-            {t('poultry.fields.gender-category.reproductive')}
-          </StyledPoultriesCarouselTitle>
-          <PoultriesCarousel
-            onEditPoultry={handleNavigateToEditPoultry}
-            onClickImage={handleNavigateToViewPoultry}
-            onViewPoultry={handleNavigateToViewPoultry}
-            poultries={poultries.reproductives}
-          />
-        </StyledPoultriesCarousel>
-      )}
-      {Boolean(poultries.male.length) && (
-        <StyledPoultriesCarousel>
-          <StyledPoultriesCarouselTitle>
-            {t('poultry.fields.gender-category.male')}
-          </StyledPoultriesCarouselTitle>
-          <PoultriesCarousel
-            onEditPoultry={handleNavigateToEditPoultry}
-            onClickImage={handleNavigateToViewPoultry}
-            onViewPoultry={handleNavigateToViewPoultry}
-            poultries={poultries.male}
-          />
-        </StyledPoultriesCarousel>
-      )}
-      {Boolean(poultries.female.length) && (
-        <StyledPoultriesCarousel>
-          <StyledPoultriesCarouselTitle>
-            {t('poultry.fields.gender-category.female')}
-          </StyledPoultriesCarouselTitle>
-          <PoultriesCarousel
-            onEditPoultry={handleNavigateToEditPoultry}
-            onClickImage={handleNavigateToViewPoultry}
-            onViewPoultry={handleNavigateToViewPoultry}
-            poultries={poultries.female}
-          />
-        </StyledPoultriesCarousel>
-      )}
+      
+      <StyledPoultries id="poultry-carousels">
+        <MicroFrontend
+          params={microFrontendParams}
+          name="BreederPoultriesPage"
+          host={POULTRY_CAROUSELS_PAGE_URL}
+          containerId="poultry-carousels"
+          callbacks={microFrontendCallbacks}
+        />
+      </StyledPoultries>
     </Main>
   )
 }
