3949a24f7792e738f9de91638ba9926212a3c31f
Eduardo Moreira <contato@eduardoem.com.br>
2022-01-23 22:53:42 -0300

    Change list poultries to micro front end (#54)

--- src/pages/ListPoultries/ListPoultries.styles.ts
+++ src/pages/ListPoultries/ListPoultries.styles.ts
@@ -1,13 +1,13 @@
 import { createMinWidthMediaQuery } from '@cig-platform/ui'
 import styled from 'styled-components'
 
-export const StyledPoultriesCarousel = styled.div``
-
-export const StyledPoultriesCarouselTitle = styled.p``
-
 export const StyledNewPoultry = styled.div`
   ${createMinWidthMediaQuery(`
     width: 50%;
     margin: 0 auto;
   `)}
+`
+
+export const StyledPoultries = styled.div`
+  overflow: hidden;
 `
