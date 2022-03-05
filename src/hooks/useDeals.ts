fce632294354e5f82e9ffdb4abebadb6af82631e
Eduardo Moreira <contato@eduardoem.com.br>
2022-03-04 23:50:01 -0300

    Chicken 552 (#75)
    * Bump @cig-platform/ui to 0.0.3
    
    * Bump @cig-platform/backoffice-bff-client to 0.0.44
    
    * Add infinit escroll
    
    * Bump @cig-paltform/backoffice-bff-client to 0.0.45

--- src/hooks/useDeals.ts
+++ src/hooks/useDeals.ts
@@ -1,7 +1,11 @@
+import { useCallback, useEffect, useState } from 'react'
 import { IAdvertising, IBreeder, IDeal, IPoultry, IPoultryRegister } from '@cig-platform/types'
-import { useEffect, useState } from 'react'
 
+import { setError, setIsLoading } from '../contexts/AppContext/appActions'
+import { useAppDispatch, useAppSelector } from '../contexts/AppContext/AppContext'
+import { selectIsLoading } from '../contexts/AppContext/appSelectors'
 import BackofficeBffService from '../services/BackofficeBffService'
+
 import useAuth from './useAuth'
 import useBreeder from './useBreeder'
 
@@ -20,20 +24,53 @@ export type Deal = {
 
 export default function useDeals({ filter }: UseDealsOptions) {
   const [deals, setDeals] = useState<Deal[]>([])
+  const [totalPages, setTotalPages] = useState(0)
+  const [page, setPage] = useState(0)
 
   const { token } = useAuth()
 
+  const dispatch = useAppDispatch()
+
+  const isLoading = useAppSelector(selectIsLoading)
+
   const breeder = useBreeder()
+
+  const handlePaginate = useCallback(() => {
+    const documentHeight = document.body.scrollHeight
+    const currentScroll = window.scrollY + window.innerHeight
+    const modifier = 200 
+    
+    if (currentScroll + modifier > documentHeight && page < totalPages - 1 && !isLoading) {
+      setPage(page + 1)
+      dispatch(setIsLoading(true))
+    }
+  }, [page, totalPages, isLoading])
 
   useEffect(() => {
     if (!token || !breeder) return
 
     (async () => {
-      const deals = await BackofficeBffService.getDeals(breeder.id, token, filter)
+      try {
+        dispatch(setIsLoading(true))
 
-      setDeals(deals)
+        const { deals, pages } = await BackofficeBffService.getDeals(breeder.id, token, filter, page)
+
+        setDeals(deals)
+        setTotalPages(pages)
+      } catch(error) {
+        dispatch(setError(error))
+      } finally {
+        dispatch(setIsLoading(false))
+      }
+      
     })()
-  }, [token, filter, breeder])
+  }, [token, filter, breeder, page, dispatch])
+
+  useEffect(() => {
+    document.addEventListener('scroll', handlePaginate)
+
+    return () => document.removeEventListener('scroll', handlePaginate)
+  }, [handlePaginate])
 
   return deals
 }
