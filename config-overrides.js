7ed3526d46db0987faf4fecc6a174cf976708ae9
Eduardo Moreira <contato@eduardoem.com.br>
2022-09-09 13:31:21 -0300

    Chicken 933 (#98)
    * Replace implemenetation per component
    
    * Bump @cig-platform/ui to 0.1.155
    
    * Fix tests suite
    
    * Fix lint suite
    
    * Fix build suite

--- config-overrides.js
+++ config-overrides.js
@@ -1,25 +1,32 @@
-module.exports = function override(config, env) {
-  config.module.rules.push({
-    test: /\.(js|jsx|ts|tsx)$/,
-    loader: require.resolve("babel-loader"),
-    options: {
-      presets: [
-        ['@babel/preset-env', {
-          targets: {
-            ie: '11',
-          },
-        }], 
-        '@babel/preset-react',
-        '@babel/preset-typescript'
-      ],
-      plugins: [
-        ["@babel/plugin-proposal-decorators", { legacy: true }],
-        "@babel/plugin-proposal-nullish-coalescing-operator",
-        'react-require'
-      ],
-    },
-    exclude: /node_modules\/(?!(@cig-platform)\/)/i,
-  });
-  
-  return config;
+module.exports = {
+  webpack: function(config, env) {
+    config.module.rules.push({
+      test: /\.(js|jsx|ts|tsx)$/,
+      loader: require.resolve("babel-loader"),
+      options: {
+        presets: [
+          ['@babel/preset-env', {
+            targets: {
+              ie: '11',
+            },
+          }], 
+          '@babel/preset-react',
+          '@babel/preset-typescript'
+        ],
+        plugins: [
+          ["@babel/plugin-proposal-decorators", { legacy: true }],
+          "@babel/plugin-proposal-nullish-coalescing-operator",
+          'react-require'
+        ],
+      },
+      exclude: /node_modules\/(?!(@cig-platform)\/)/i,
+    });
+    
+    return config;
+  },
+  jest: function(config) {
+    config.moduleNameMapper['react-family-tree'] = '<rootDir>/src/transform.js'
+
+    return config;
+  },
 }
--- src/pages/ManageTree/ManageTree.tsx
+++ src/pages/ManageTree/ManageTree.tsx
@@ -1,43 +1,14 @@
-import { PoultryGenderEnum } from '@cig-platform/enums'
 import { IPoultry } from '@cig-platform/types'
+import { Tree } from '@cig-platform/ui'
 import useBreeder from 'hooks/useBreeder'
-import { useCallback, useEffect, useMemo, useState } from 'react'
-import ReactFamilyTree from 'react-family-tree'
+import { useCallback, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import ContentSearchService from 'services/ContentSearchService'
-import type { ExtNode } from 'relatives-tree/lib/types'
-import { BsGenderMale, BsGenderFemale } from 'react-icons/bs'
 
-import { TreeCrest, TreeDewlap, TreeGender, TreeInfo, TreeItem, TreeName, TreeTail, Container, TreeItemContainer, TreeItemExpand, TreeItemAddMom, TreeItemAddDad } from './ManagreTree.styles'
 import { useAppDispatch } from 'contexts/AppContext/AppContext'
 import { setIsLoading } from 'contexts/AppContext/appActions'
 import BackofficeBffService from 'services/BackofficeBffService'
 import useAuth from 'hooks/useAuth'
-
-const WIDTH = 150
-const HEIGHT = 150
-
-type NodeFamiliar = {
-  id: string;
-  type: string;
-}
-
-type Node = {
-  id: string;
-  gender: any;
-  parents: NodeFamiliar[];
-  children: NodeFamiliar[];
-  siblings: NodeFamiliar[];
-  spouses: NodeFamiliar[];
-}
-
-function getNodeStyle({ left, top }: Readonly<ExtNode>) {
-  return {
-    width: WIDTH,
-    height: HEIGHT,
-    transform: `translate(${left * (WIDTH / 2)}px, ${top * (HEIGHT / 2)}px)`,
-  }
-}
 
 const ManageTreePage = () => {
   const { poultryId } = useParams<{ poultryId: string }>()
@@ -80,53 +51,6 @@ const ManageTreePage = () => {
       }
     })()
   }, [poultryId, breeder?.id])
-  
-  const nodes = useMemo<Node[]>(() => poultries.map((poultry) => {
-    const parents = []
-
-    if (poultry.momId && poultries.some(p => p.id === poultry.momId)) {
-      parents.push({
-        id: poultry.momId,
-        type: 'blood'
-      })
-    }
-
-    if (poultry.dadId && poultries.some(p => p.id === poultry.dadId)) {
-      parents.push({
-        id: poultry.dadId,
-        type: 'blood'
-      })
-    }
-
-    const children = poultries.filter(p => p.momId === poultry.id || p.dadId === poultry.id).map(p => ({
-      id:p.id,
-      type: 'blood'
-    }))
-
-    const siblings = poultries.filter(p => ((p.momId === poultry.momId && p.momId) || (p.dadId === poultry.dadId && p.dadId)) && p.id !== poultry.id)
-
-    const spouses = children.map(c => {
-      const p = poultries.find(p => p.id === c.id)
-      const spouseId = p?.momId === poultry.id ? p?.dadId : p?.momId
-      
-      if (!spouseId) return undefined
-
-      const spouse = poultries.find(p => p.id === spouseId)
-
-      if (!spouse) return undefined
-
-      return { type: 'married', id: spouse.id }
-    }).filter(Boolean) as NodeFamiliar[]
-
-    return {
-      id: poultry.id,
-      gender: poultry.gender === PoultryGenderEnum.Male ? 'male' : 'female',
-      parents,
-      spouses,
-      children,
-      siblings
-    }
-  }), [poultries])
 
   const handleClickExpandButton = useCallback(async (poultryId: string) => {
     dispatch(setIsLoading(true))
@@ -196,56 +120,13 @@ const ManageTreePage = () => {
   if (!poultries.length) return null
 
   return (
-    <Container>
-      <ReactFamilyTree
-        nodes={nodes as any}
-        rootId={poultryId ?? ''}
-        width={WIDTH}
-        height={HEIGHT}
-        renderNode={(node) => {
-          const poultry = poultries.find(p => p.id === node.id)
-
-          if (!poultry) return null
-
-          const shouldRenderExpandButton = Boolean(poultry.dadId || poultry.momId) && poultries.every(p => poultry.dadId ? p.id !== poultry.dadId : p.id !== poultry.momId)
-          const shouldRenderAddDadButton = !poultry.dadId
-          const shouldRenderAddMomButton = !poultry.momId
-
-          return (
-            <TreeItemContainer key={node.id} style={getNodeStyle(node)}>
-              <TreeItem>
-                {shouldRenderAddDadButton && (
-                  <TreeItemAddDad onClick={() => handleAddDad(poultry.id)}>
-                    + <BsGenderMale />
-                  </TreeItemAddDad>
-                )}
-
-                {shouldRenderAddMomButton && (
-                  <TreeItemAddMom onClick={() => handleAddMom(poultry.id)}>
-                    + <BsGenderFemale />
-                  </TreeItemAddMom>
-                )}
-
-                {shouldRenderExpandButton && (
-                  <TreeItemExpand onClick={() => handleClickExpandButton(poultry.id)}>+</TreeItemExpand>
-                )}
-
-                <TreeName>{poultry?.name}</TreeName>
-                <TreeGender>{poultry?.gender}</TreeGender>
-
-                {Boolean(poultry?.dewlap || poultry?.crest || poultry?.tail) && (
-                  <TreeInfo>
-                    <TreeDewlap>{poultry?.dewlap}</TreeDewlap>
-                    <TreeCrest>{poultry?.crest}</TreeCrest>
-                    <TreeTail>{poultry?.tail}</TreeTail>
-                  </TreeInfo>
-                )}
-              </TreeItem>
-            </TreeItemContainer>
-          )
-        }}
-      />
-    </Container>
+    <Tree
+      poultries={poultries}
+      onAddDad={handleAddDad}
+      onAddMom={handleAddMom}
+      onExpand={handleClickExpandButton}
+      rootId={poultryId ?? ''}
+    />
   )
 }
 
