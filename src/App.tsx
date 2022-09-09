d5855106f0118fc7bef5c4a85ed9c5384f3cd4e1
Eduardo Moreira <contato@eduardoem.com.br>
2022-09-09 18:06:42 -0300

    Replace reload page per refetch (#100)

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,22 @@ import Router from './router'
 import Container from './components/Container/Container'
 import { BreederProvider } from './contexts/BreederContext/BreederContext'
 import { AppProvider } from './contexts/AppContext/AppContext'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <AppProvider>
-        <BreederProvider>
-          <Container>
-            <Router />
-          </Container>
-        </BreederProvider>
-      </AppProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AppProvider>
+          <BreederProvider>
+            <Container>
+              <Router />
+            </Container>
+          </BreederProvider>
+        </AppProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
---
+++ src/hooks/usePoultryAdvertising.ts
@@ -0,0 +1,30 @@
+import { IAdvertising, IPoultry, IPoultryImage, IPoultryRegister } from '@cig-platform/types'
+import { useQuery } from 'react-query'
+
+import BackofficeBffService from 'services/BackofficeBffService'
+import useAuth from './useAuth'
+import useBreeder from './useBreeder'
+
+type Data = {
+  poultry: IPoultry & {
+    images: IPoultryImage[];
+    registers: IPoultryRegister[];
+};
+  advertisings: IAdvertising[];
+}
+
+export default function usePoultryAdvertising(poultryId: string) {
+  const { token } = useAuth()
+  
+  const breeder = useBreeder()
+
+  const { data, refetch, isLoading } = useQuery<Data>(
+    ['getPoultryAdvertising', breeder?.id, poultryId, token],
+    () => BackofficeBffService.getPoultry(breeder?.id ?? '', poultryId, token),
+    {
+      enabled: Boolean(token && poultryId && breeder?.id),
+    }
+  )
+  
+  return { advertising: data?.advertisings?.[0], refetch, isLoading }
+}
