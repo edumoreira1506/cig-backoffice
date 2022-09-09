417c9f52072e3559bdc57d1e5a26eb4e1551df5b
edumoreira1506 <contato@eduardoem.com.br>
2022-09-09 20:34:50 -0300

    Change useDeals to use data-helper

--- src/hooks/useDeals.ts
+++ src/hooks/useDeals.ts
@@ -1,13 +1,14 @@
 import { useCallback, useEffect, useState } from 'react'
 import { IAdvertising, IBreeder, IDeal, IPoultry, IPoultryRegister } from '@cig-platform/types'
 
-import { setError, setIsLoading } from '../contexts/AppContext/appActions'
+import { setIsLoading } from '../contexts/AppContext/appActions'
 import { useAppDispatch, useAppSelector } from '../contexts/AppContext/AppContext'
 import { selectIsLoading } from '../contexts/AppContext/appSelectors'
 import BackofficeBffService from '../services/BackofficeBffService'
 
 import useAuth from './useAuth'
 import useBreeder from './useBreeder'
+import { useData } from '@cig-platform/data-helper'
 
 type UseDealsOptions = {
   filter: 'SELLER'| 'BUYER';
@@ -20,6 +21,19 @@ export type Deal = {
   breeder: IBreeder;
   measurementAndWeight: IPoultryRegister[];
   mainImage?: string;
+}
+
+type Data = {
+  deals: {
+    poultry: IPoultry;
+    advertising: IAdvertising;
+    deal: IDeal;
+    breeder: IBreeder;
+    measurementAndWeight: IPoultryRegister[];
+    mainImage?: string;
+  }[];
+  pages: number;
+  ok: true;
 }
 
 export default function useDeals({ filter }: UseDealsOptions) {
@@ -47,30 +61,41 @@ export default function useDeals({ filter }: UseDealsOptions) {
   }, [page, totalPages, isLoading])
 
   useEffect(() => {
-    if (!token || !breeder) return
-
-    (async () => {
-      try {
-        dispatch(setIsLoading(true))
-
-        const { deals, pages } = await BackofficeBffService.getDeals(breeder.id, token, filter, page)
-
-        setDeals(deals)
-        setTotalPages(pages)
-      } catch(error) {
-        dispatch(setError(error))
-      } finally {
-        dispatch(setIsLoading(false))
-      }
-      
-    })()
-  }, [token, filter, breeder, page, dispatch])
-
-  useEffect(() => {
     document.addEventListener('scroll', handlePaginate)
 
     return () => document.removeEventListener('scroll', handlePaginate)
   }, [handlePaginate])
+
+  const { data, isLoading: isLoadingGetDealsRequest } = useData<Data>(
+    'getDeals', 
+    () => BackofficeBffService.getDeals(breeder?.id ?? '', token, filter, page),
+    [
+      breeder?.id,
+      token,
+      filter,
+      ...(page ? [page] : [])
+    ],
+    {}
+  )
+
+  useEffect(() => {
+    if (!data?.deals?.length) return
+
+    setDeals(prevDeals => [
+      ...prevDeals,
+      ...data?.deals?.filter(a =>
+        prevDeals.every(pA => pA.deal.id !== a.deal.id)
+      ) ?? []
+    ])
+  }, [data?.deals])
+
+  useEffect(() => {
+    dispatch(setIsLoading(isLoadingGetDealsRequest))
+  }, [isLoadingGetDealsRequest, dispatch])
+
+  useEffect(() => {
+    setTotalPages(data?.pages ?? 0)
+  }, [data?.pages])
 
   return deals
 }
